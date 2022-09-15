d8d69c3689f02d271745c8532737a5a385ec5994
lapinRagnar <lapinragnar@gmail.com>
2022-09-15 23:13:26 +0200

    layout - header suite

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,29 +1,51 @@
 <template>
+
   <q-layout view="hHr lpR fFf">
 
-    <q-header class="bg-primary text-white">
+    <!-- header -->
+    <q-header class="bg-pink-2 text-white q-py-lg">
       <q-toolbar>
         <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-          </q-avatar>
-          Title
+
+          <q-icon name="public" to="/" clickable size="lg" />
+
+          MonSA
+
         </q-toolbar-title>
 
-        <q-btn dense flat round icon="menu" @click="toggleRightDrawer" />
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer"  />
+
       </q-toolbar>
     </q-header>
 
+    <q-header elevated class="bg-purple">
+      <q-toolbar>
+        <q-btn flat round dense icon="menu" class="q-mr-sm" @click="toggleLeftDrawer" />
+        <q-space ></q-space>
+        <q-btn flat round dense icon="search" class="q-mr-xs" />
+        <q-btn flat round dense icon="group_add" />
+      </q-toolbar>
+      <q-toolbar inset class="bg-blue ">
+        <q-toolbar-title>
+          <span><strong>Quasar</strong> Framework</span>
+        </q-toolbar-title>
+        <q-btn dense flat round icon="menu" @click="toggleRightDrawer"  />
+      </q-toolbar>
+    </q-header>
+
+    <!-- sidebar gauche -->
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
       <!-- drawer content -->
     </q-drawer>
 
+    <!-- sidebar droite -->
     <q-drawer v-model="rightDrawerOpen" side="right" behavior="mobile" bordered>
       <!-- drawer content -->
     </q-drawer>
 
+    <!-- contenu au milieu -->
     <q-page-container>
       <router-view />
     </q-page-container>
@@ -40,6 +62,7 @@
     </q-footer>
 
   </q-layout>
+  
 </template>
 
 <script>
