8b2d410141177b852d976ee6e1d94a55fafc0009
lapinRagnar <lapinragnar@gmail.com>
2022-09-15 19:53:40 +0200

    1 - premier layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <h1>Bonjour - mon super app</h1>
   </q-page>
 </template>
 
